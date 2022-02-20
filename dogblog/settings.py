052e673cbc7cb9bf9408ef9ba07ad71d10a8b33e
Gabriela Di Salvatore-Bosshard <gabrieladisalvatore@gmail.com>
2022-02-20 15:22:38 +0000

    set debug to true

--- dogblog/settings.py
+++ dogblog/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG=True
 
 ALLOWED_HOSTS = ['simple-dog-blog.herokuapp.com', 'localhost', '127.0.0.1']
 
